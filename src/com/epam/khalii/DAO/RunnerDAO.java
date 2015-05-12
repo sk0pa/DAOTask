c4badfd6669c96788a190e7c7f5a4d7e798598a2
Anhelina Khalii <anhelina_khalii@epam.com>
2015-05-12 19:50:13 +0300

    Add all sql queries

--- src/com/epam/khalii/DAO/RunnerDAO.java
+++ src/com/epam/khalii/DAO/RunnerDAO.java
@@ -14,10 +14,12 @@ import java.util.List;
  * Created by Anhelina_Khalii on 12.05.2015.
  */
 public class RunnerDAO extends AbstractDAO <Actor, Film> {
-    {
-    }
-    public static final String SQL_GET_LAST_FILMS = "select * from film where date > (((CURRENT_DATE - INTERVAL (DAY(CURRENT_DATE())-1) DAY) - INTERVAL (MONTH(CURRENT_DATE()) MONTH)-1)- INTERVAL 1 YEAR )";
 
+    public static final String SQL_GET_LAST_FILMS = "select film_name, film_date, film_country from film where film_date > (((CURRENT_DATE - INTERVAL (DAY(CURRENT_DATE())-1) DAY) - INTERVAL (MONTH(CURRENT_DATE())-1) MONTH) - INTERVAL 1 YEAR )";
+    public static final String SQL_GET_ACTORS_FROM_FILM = "select actor_name, actor_birth from actor, film_actor where film_actor.actor_id = actor.actor_id AND film_actor.film_id = %d";
+    public static final String SQL_GET_ACTORS_WITH_LONG_FILMOGRAPHY ="select actor.actor_id, actor.actor_name, actor.actor_birth, COUNT(film_id) as filmography from actor, film_actor where film_actor.actor_id = actor.actor_id GROUP BY actor.actor_id HAVING count(film_id) > %d ";
+    public static final String SQL_GET_PRODUCERS = "select DISTINCT actor.actor_name, actor.actor_birth from actor RIGHT JOIN film_producer ON film_producer.producer_id = actor.actor_id ";
+    public static final String SQL_GET_OLD_FILMS = "delete from film where film_date < (CURRENT_DATE - INTERVAL %d YEAR) ";
 
     @Override
     public List<Film> findFilms() throws SQLException{
